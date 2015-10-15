df8278f65639167c16b09765819fcdeaa5edac58
Mephisto-Pheies <mephistommm@gmail.com>
2015-10-15 08:47:06 +0800

    fix vim and update vimrc and zshrc

--- ready.sh
+++ ready.sh
@@ -6,15 +6,13 @@ apt-get install -y git zsh vim
 
 #vim and vimrc and vundle
 mkdir ~/.vim
-cp ./src/vimrc ~/.vim/vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim source ~/.vim/vimrc
-vim PluginInstall 
+cp ./src/vimrc ~/.vim/vimrc
+vim +source~/.vim/vimrc +PluginInstall +qall
 
 
 #zsh and oh my zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-mv ~/.zshrc ~/.zshrc.default
 cp ./src/zshrc ~/.zshrc
 chsh -s /bin/zsh
 /bin/zsh
