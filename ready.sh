d5f4631ec6b6d2a55aa5c3fcf95c0fb784cb4232
Mephisto-Pheies <mephistommm@gmail.com>
2015-10-15 11:13:36 +0800

    add python3 and pip2 pip3 and w3m

--- ready.sh
+++ ready.sh
@@ -5,15 +5,23 @@ apt-get install -y git zsh vim
 
 
 #vim and vimrc and vundle
-mkdir ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./src/vimrc ~/.vim/vimrc
-vim +source~/.vim/vimrc +PluginInstall +qall
+mkdir ~/.vim\
+    && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim \
+    && cp ./src/vimrc ~/.vim/vimrc \
+    && vim +source~/.vim/vimrc +PluginInstall +qall
 
 
 #zsh and oh my zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ./src/zshrc ~/.zshrc
-chsh -s /bin/zsh
-/bin/zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh\
+    && cp ./src/zshrc ~/.zshrc\
+    && chsh -s /bin/zsh
+
+# install python3
+# install python-pip and python3-pip
+apt-get install python3 \
+    && apt-get install -y python-pip python3-pip
+
+# install w3m
+apt-get install -y w3m
+
 
