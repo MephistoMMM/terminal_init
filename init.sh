a59036a37466bab1e256fa4e0b27dcb52f17ebc4
mpsss <mephistommm@gmail.com>
2015-11-10 18:10:00 +0800

    add jsfmt

--- init.sh
+++ init.sh
@@ -5,15 +5,20 @@ apt-get install -y git zsh vim
 
 
 #vim and vimrc and vundle
+ls ~/.vim || rm -rf ~/.vim
 mkdir ~/.vim\
-    && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim \
     && cp ./src/vimrc ~/.vim/vimrc \
+    && chown -R $(USER):$(USER) ~/.vim
+    && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim \
     && vim +source~/.vim/vimrc +PluginInstall +qall
 
 
 #zsh and oh my zsh
+ls ~/.oh-my-zsh || rm -rf ~/.oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh\
+    && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting\
     && cp ./src/zshrc ~/.zshrc\
+    && chown $(USER):$(USER) ~/.zshrc
     && chsh -s /bin/zsh
 
 # install python3
