fd8e9b04de247a5c78476dd4efb0accd4e6376f1
mpsss <mephistommm@gmail.com>
2015-12-10 12:59:08 +0800

    over docker

--- init.d/docker.sh
+++ init.d/docker.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 # 
 # This script install docker 
+# arch user should start docker server by systemctl
 #
 # CreateDate: 2015/12/10
 # ChangeDate: 2015/12/10
 # Author Mephis Pheies
 # Email mephistommm@gmail.com
+source $GENPATH/lib/package.sh
+
+is_archlinux
+
+if [ $? -eq 0 ]; then
+    install_packages docker
+else
+    wget -qO- https://get.docker.com/gpg | sudo apt-key add -
+    wget -qO- https://get.docker.com/ | sh
+fi
+
+usermod -aG docker $INIT_USER
+    
 
 
