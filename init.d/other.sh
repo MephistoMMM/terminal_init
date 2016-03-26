da4492bac83e07a34838f12970833f6fcaa72e6e
mpsss <mephistommm@gmail.com>
2016-03-26 18:02:07 +0800

    add nodejs

--- init.d/other.sh
+++ init.d/other.sh
@@ -6,6 +6,7 @@
 # python-pip
 # python3-pip
 # w3m
+# nodejs 5.x
 # ----------
 #
 # CreateDate: 2015/11/30
@@ -18,3 +19,10 @@ source $GENPATH/lib/package.sh
 install_packages python3 
 install_packages python3-pip python-pip
 install_packages w3m
+
+if [ $IS_ARCHLINUX == "1" ] ; then 
+    curl -sL https://deb.nodesource.com/setup_5.x | /bin/bash -c -
+    install_packages nodejs
+else
+    install_packages nodejs npm
+fi
