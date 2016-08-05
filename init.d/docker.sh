d5ea3999f63b627ceef4340ca6c9d77fcc2a5b64
Mephis Pheies <mephistommm@gmail.com>
2016-08-05 15:46:30 +0800

    update docker compose

--- init.d/docker.sh
+++ init.d/docker.sh
@@ -21,7 +21,7 @@ fi
 gpasswd -a $INIT_USER docker
     
 #to install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 
