95b28abeb9db873af5534b20a6939f24519d81b5
mpsss <mephistommm@gmail.com>
2016-09-21 09:16:48 +0800

    fixbug: ubuntu install nodejs error

--- init.d/other.sh
+++ init.d/other.sh
@@ -6,7 +6,7 @@
 # python-pip
 # python3-pip
 # w3m
-# nodejs 5.x
+# nodejs 6.x
 # ----------
 #
 # CreateDate: 2015/11/30
@@ -20,9 +20,9 @@ install_packages python3
 install_packages python3-pip python-pip
 install_packages w3m
 
-if [ $IS_ARCHLINUX == "1" ] ; then 
-    curl -sL https://deb.nodesource.com/setup_5.x | /bin/bash -c -
-    install_packages nodejs
-else
+if [ $IS_ARCHLINUX == "1" ] ; then
     install_packages nodejs npm
+else
+    curl -sL https://deb.nodesource.com/setup_6.x | /bin/bash -c -
+    install_packages nodejs
 fi
