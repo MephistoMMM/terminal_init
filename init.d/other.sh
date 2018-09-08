899f5591e12e72cf8321a7bd7291d1d2c3d56c25
Mephis Pheies <mephistommm@gmail.com>
2018-09-08 16:25:56 +0800

    install tree and htop in other.sh

--- init.d/other.sh
+++ init.d/other.sh
@@ -5,8 +5,9 @@
 # python3 
 # python-pip
 # python3-pip
-# w3m
 # nodejs 7.x
+# tree
+# htop
 # ----------
 #
 # CreateDate: 2015/11/30
@@ -19,7 +20,6 @@ source $GENPATH/lib/package.sh
 if [ $SYSTYPE != 2 ] ; then
     install_packages python3 
     install_packages python3-pip python-pip
-    install_packages w3m
 
     if [ $SYSTYPE == 1 ] ; then
         install_packages nodejs npm
@@ -30,5 +30,7 @@ if [ $SYSTYPE != 2 ] ; then
 else
     ./init.d/centos_python3.sh
     curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
-    install_packages nodejs w3m
+    install_packages nodejs
 fi
+
+install_packages tree htop
